87776a6475583066cf046664f35a69c4cc7333a9
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-11-08 21:20:32 +0200

    feat: change header name to hassan.dev

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
+          <title>Hassan.dev</title>
+          <meta property="og:title" content="Hassan.dev" key="title" />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@200&amily=Ubuntu+Mono&family=Poppins:wght@100&display=swap&family=Orbitron:wght@900&display=swap"
             rel="stylesheet"
