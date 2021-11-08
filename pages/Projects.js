dc5e8a665ea9ec1557b09c3528708311bf97163d
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-11-08 21:20:50 +0200

    feat: add Project page

---
+++ pages/Projects.js
@@ -0,0 +1,38 @@
+import Navbar from "../components/Navbar/Nav";
+import ProjectItems from "../components/Projects/ProjectItems";
+
+const ProjectsPage = () => {
+  return (
+    <div style={{ maxWidth: "1600px", fontFamily: "poppins" }}>
+      <Navbar />
+      <div
+        className="hero min-h-screen bg-fixed"
+        style={{
+          backgroundImage: `url("https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="text-center hero-content text-neutral-content">
+          <div className="max-w-md">
+            <h1
+              className="mb-5 text-8xl font-bold"
+              style={{ fontFamily: "IBM Plex Mono,monospace" }}
+            >
+              Projects
+            </h1>
+
+            <p className="mb-5 text-xl text-primary font-extrabold w-full">
+              Every project is an opportunity to learn, to figure out problems
+              and challenges, to invent and reinvent.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="my-20">
+        <ProjectItems start={0} end={-1} />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsPage;
