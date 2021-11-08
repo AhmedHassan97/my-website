f6954a6029d23fe66f3255665f187b8cd76d6cba
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-11-08 21:20:08 +0200

    fix: fix index Styling

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,17 @@
 import Navbar from "../components/Navbar/Nav";
 import SkillsComponent from "../components/Skills";
-import React, { useRef } from "react";
-import { useIntersectionObserver } from "usehooks-ts";
 import Profile from "../components/Profile";
-
+import Projects from "../components/Projects/Projects";
+import Experiences from "../components/Experience";
 export default function Home() {
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.5,
-  };
-
-  const ref = useRef(null);
-  const entry = useIntersectionObserver(ref, options);
-  const isProfileVisible = !!entry?.isIntersecting;
-
-  const refAbilities = useRef(null);
-  const entryAbilities = useIntersectionObserver(refAbilities, options);
-  const isAbilitiesVisible = !!entryAbilities?.isIntersecting;
-
-  const refExperience = useRef(null);
-  const entryExperience = useIntersectionObserver(refExperience, options);
-  const isExperienceVisible = !!entryExperience?.isIntersecting;
-
-  const refProjects = useRef(null);
-  const entryProjects = useIntersectionObserver(refProjects, options);
-  const isProjectsVisible = !!entryProjects?.isIntersecting;
-
   return (
-    <div
-      style={{ fontFamily: "poppins" }}
-      className="text-base-content font-medium text-2xl "
-    >
-      <div className="bg-base-100 transition duration-100 ease-linear w-full">
-        <Navbar
-          isProfileVisible={isProfileVisible}
-          isAbilitiesVisible={isAbilitiesVisible}
-          isExperienceVisible={isExperienceVisible}
-          isProjectsVisible={isProjectsVisible}
-        />
-        <div ref={ref}>
-          <Profile id="abilities" />
-        </div>
-
-        <div
-          className="bg-base-200 transition duration-100 ease-linear"
-          ref={refAbilities}
-        >
-          <SkillsComponent />
-        </div>
-
-        {/* Experience */}
-        <div
-          className="bg-base-100 transition duration-100 ease-linear"
-          ref={refExperience}
-        >
-          <SkillsComponent />
-        </div>
-
-        {/* Projects */}
-        <div
-          className="bg-base-200 transition duration-100 ease-linear"
-          ref={refProjects}
-        >
-          <SkillsComponent />
-        </div>
+    <div>
+      <div>
+        <Navbar />
+        <Profile />
+        <SkillsComponent />
+        <Experiences />
+        <Projects start={0} end={2} />
       </div>
     </div>
   );
