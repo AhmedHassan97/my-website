d20a5e92303054935a100ea074a62c4cb4beca48
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-11-08 21:18:26 +0200

    fix: fix Nav styling

--- components/Navbar/Nav.js
+++ components/Navbar/Nav.js
@@ -1,9 +1,11 @@
 import { Fragment, useEffect, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon, MoonIcon } from "@heroicons/react/outline";
-import Image from "next/image";
+import { MenuIcon, XIcon, BackspaceIcon } from "@heroicons/react/outline";
+import Link from "next/link";
 import { useTheme } from "next-themes";
-
+import Scrollspy from "react-scrollspy";
+import { useRouter } from "next/router";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 const themes = ["halloween", "light", "luxury", "corporate", "forest"];
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -11,37 +13,33 @@ function classNames(...classes) {
 
 const Navbar = (props) => {
   const { theme, setTheme } = useTheme();
+  const router = useRouter();
+
   const [activeNavigation, setActiveNavigation] = useState([
-    { name: "Profile", href: "#", current: true },
-    { name: "Experience", href: "#", current: false },
-    { name: "Projects", href: "#", current: false },
-    { name: "Abilities", href: "#", current: false },
+    { name: "Profile", href: "#profile" },
+    { name: "Skills", href: "#skills" },
+    { name: "Experience", href: "#experience" },
+    { name: "Projects", href: "#projects" },
   ]);
-
   useEffect(() => {
-    setActiveNavigation([
-      { name: "Profile", href: "#", current: props.isProfileVisible },
-      { name: "Abilities", href: "#", current: props.isAbilitiesVisible },
-      { name: "Experience", href: "#", current: props.isExperienceVisible },
-      { name: "Projects", href: "#", current: props.isProjectsVisible },
-    ]);
-  }, [
-    props.isProfileVisible,
-    props.isAbilitiesVisible,
-    props.isExperienceVisible,
-    props.isProfileVisible,
-  ]);
-
+    console.log(router.pathname);
+  });
   return (
     <Disclosure
       as="nav"
-      style={{ fontFamily: "Ubuntu Mono" }}
-      className="sticky top-0 bg-base-100"
+      className="sticky top-0 bg-base-100 z-50 text-base-content font-medium text-xl "
+      style={{ fontFamily: "poppins" }}
     >
       {({ open }) => (
         <>
-          <div className=" px-2 lg:px-8 mx-auto" style={{ maxWidth: "1600px" }}>
-            <div className="relative flex items-center justify-between h-16">
+          <div
+            className=" px-2 lg:px-8 mx-auto font-semibold text-xl my-2"
+            style={{ maxWidth: "1600px" }}
+          >
+            <div
+              className="relative flex items-center justify-between h-16"
+              style={{ fontFamily: "IBM Plex Mono,monospace" }}
+            >
               <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-primary hover:bg-base-300">
@@ -54,38 +52,65 @@ const Navbar = (props) => {
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center lg:items-stretch lg:justify-start">
-                <div className="flex-shrink-0 flex xl:mr-auto rounded-box bg-primary">
-                  <h1
-                    className=" font-extrabold text-2xl text-primary-content px-2"
-                    style={{ fontFamily: "Orbitron" }}
-                  >
-                    H
-                  </h1>
-                </div>
-                <div className="hidden lg:block  ml-auto lg:ml-auto">
-                  <div className="flex space-x-4">
-                    {activeNavigation?.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "border-b-2 border-primary text-primary"
-                            : "text-md hover:text-primary-focus"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                {router.pathname === "/" ? (
+                  <div className="flex-shrink-0 flex xl:mr-auto rounded-box bg-primary">
+                    <h1
+                      className=" font-extrabold text-2xl text-primary-content px-2"
+                      style={{ fontFamily: "Orbitron" }}
+                    >
+                      <Link href="/">
+                        <a>H</a>
+                      </Link>
+                    </h1>
                   </div>
-                </div>
+                ) : (
+                  <div className="flex-shrink-0 flex xl:mr-auto rounded-box bg-primary">
+                    <h1
+                      className=" font-extrabold text-2xl text-primary-content px-2"
+                      style={{ fontFamily: "Orbitron" }}
+                    >
+                      <Link href="/">
+                        <a>H</a>
+                      </Link>
+                    </h1>
+                  </div>
+                )}
+                {router.pathname === "/" ? (
+                  <div className="hidden lg:block  ml-auto lg:ml-auto">
+                    <div className="flex space-x-4">
+                      <Scrollspy
+                        items={["profile", "skills", "experience", "projects"]}
+                        currentClassName="border-b-2 border-primary text-primary"
+                      >
+                        {activeNavigation?.map((item) => (
+                          <a
+                            href={item.href}
+                            key={item.name}
+                            className="text-md hover:text-primary-focus mx-2"
+                            // aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </Scrollspy>
+                    </div>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3">
                   <div>
-                    <Menu.Button className="flex text-sm rounded-full hover:bg-base-300 p-2 ">
+                    <Menu.Button
+                      className="flex text-sm rounded-full hover:bg-primary hover:text-primary-content p-2 "
+                      style={{
+                        boxShadow: "-3px 5px #33332d",
+                        border: "3px solid",
+                        fontWeight: "600",
+                      }}
+                    >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -99,7 +124,7 @@ const Navbar = (props) => {
                           d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
                         ></path>
                       </svg>
-                      <h1 className="text-center text-xl hidden lg:flex">
+                      <h1 className="text-center text-lg hidden lg:flex font-semibold">
                         Change Theme
                       </h1>
                       <svg
@@ -128,7 +153,7 @@ const Navbar = (props) => {
                               className={
                                 theme === themeName
                                   ? "bg-primary-focus rounded-box text-left px-2 p-1 text-primary-content m-1 "
-                                  : " text-left px-2 p-1 m-1"
+                                  : " text-left px-2 p-1 m-1 font-bold"
                               }
                               onClick={() => setTheme(themeName)}
                             >
@@ -143,27 +168,24 @@ const Navbar = (props) => {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className=" lg:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 flex flex-col">
-              {activeNavigation?.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "border-b-2 border-primary  text-primary"
-                      : "text-md hover:text-primary-focus",
-                    "px-3 font-medium w-auto"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
+          {router.pathname === "/" ? (
+            <Disclosure.Panel className=" lg:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1 flex flex-col">
+                {activeNavigation?.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className="text-md hover:text-primary-focus px-3 font-extrabold w-auto"
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          ) : (
+            <></>
+          )}
         </>
       )}
     </Disclosure>
