22a4daaab65de34c5cc2fa656451395fb90df92d
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-11-12 02:15:57 +0200

    feat: add new animation to about

--- components/Profile/About.js
+++ components/Profile/About.js
@@ -1,7 +1,8 @@
 import { saveAs } from "file-saver";
 import Image from "next/image";
-import useTypingEffect from "use-typing-effect";
-
+// import useTypingEffect from "use-typing-effect";
+import TitleAnimated from "../TitleAnimated/TitleAnimated";
+import { motion } from "framer-motion";
 const About = () => {
   const saveFile = () => {
     saveAs(
@@ -9,10 +10,10 @@ const About = () => {
       "Resume.pdf"
     );
   };
-  const heading = useTypingEffect(["Hi,I am Ahmed Hassan"], {
-    loop: false,
-    playbackRate: 2,
-  });
+  // const heading = useTypingEffect(["Hi,I am Ahmed Hassan"], {
+  //   loop: false,
+  //   playbackRate: 2,
+  // });
 
   return (
     <div className="flex flex-col max-w-4xl">
@@ -22,7 +23,19 @@ const About = () => {
           className="mb-5 text-7xl font-extrabold  "
           style={{ fontFamily: "IBM Plex Mono,monospace" }}
         >
-          {heading}
+          {/* {heading} */}
+          <TitleAnimated
+            text={[
+              `Hi, I'm <span >Ahmed</span >`,
+              `Hi, I'm <span >Ahmed Hassan</span >`,
+              `Hi, I'm <span >Ahmed Hassan</span >`,
+              `Nice to meet <span >you!</span >`,
+              `Hi, I'm <span >Ahmed</span>`,
+            ]}
+            textSize="text-5xl sm:text-6xl lg:text-7xl"
+            breakText={true}
+            extraStyle="md:pt-14 lg:pt-16"
+          />
         </h1>
       </div>
       {/* Body */}
@@ -30,12 +43,17 @@ const About = () => {
         <p className="pb-3">\//ABOUT</p>
       </div>
       <div>
-        <p className="mb-5 text-base-content font-bold text-justify leading-8">
+        <motion.p
+          initial={{ y: "-100vh", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1.5 }}
+          className="mb-5 text-base-content font-bold text-justify leading-8"
+        >
           I am a Fullstack engineer with a proven track record of designing and
           implementing innovative user-centric websites. I have been working in
           the field for the past four years developing complex applications in
           Web Development.
-        </p>
+        </motion.p>
       </div>
       <div style={{ fontFamily: "IBM Plex Mono,monospace" }}>
         <button
